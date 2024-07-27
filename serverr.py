f28889ddfcc02e95761c2781741d9598d6847e1a
GLACIER <vdas9952@gmail.com>
2024-07-27 13:08:14 +0530

    Update serverr.py

--- serverr.py
+++ serverr.py
@@ -204,7 +204,7 @@ def start_attack_reply(message, target, port, time):
     user_info = message.from_user
     username = user_info.username if user_info.username else user_info.first_name
     
-    response = f"{username}, 𝐀𝐓𝐓𝐀𝐂𝐊 𝐒𝐓𝐀𝐑𝐓𝐄𝐃.🔥🔥\n\n𝐓𝐚𝐫𝐠𝐞𝐭: {target}\n𝐏𝐨𝐫𝐭: {port}\n𝐓𝐢𝐦𝐞: {time} 𝐒𝐞𝐜𝐨𝐧𝐝𝐬\n𝐌𝐞𝐭𝐡𝐨𝐝: BGMI"
+    response = f"{username}, __ATTACK STARTED__\n\n𝐓𝐚𝐫𝐠𝐞𝐭: {target}\n**PORT**: {port}\n**TIME**: {time} **SECONDS**\n**VERSION**: BGMI"
     bot.reply_to(message, response)
 
 # Dictionary to store the last time each user ran the /bgmi command
